449dbaceb97d84d0b3fe8caffcab3ae697ac7268
eswaribala <parameswaribala@gmail.com>
2020-11-23 23:31:22 +0530

    v1

---
+++ spring-kafka-java-config/src/main/java/com/virtusa/banking/controller/KafkaProducerController.java
@@ -0,0 +1,40 @@
+package com.virtusa.banking.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.virtusa.banking.model.User;
+import com.virtusa.banking.service.KafKaProducerService;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaProducerController {
+	private final KafKaProducerService producerService;
+
+	@Autowired
+	public KafkaProducerController(KafKaProducerService producerService) {
+		this.producerService = producerService;
+	}
+
+	@PostMapping(value = "/publish")
+	public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+		this.producerService.sendMessage(message);
+	}
+	
+	@PostMapping(value = "/createUser")
+	public void sendMessageToKafkaTopic(
+			@RequestParam("userId") long userId, 
+			@RequestParam("firstName") String firstName,
+			@RequestParam("lastName") String lastName) {
+		
+		User user = new User();
+		user.setUserId(userId);
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		
+		this.producerService.saveCreateUserLog(user);
+	}
+}
