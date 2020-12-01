0a24bfebbaa1a575514d2ab87f6f2ec5236fe2c4
eswaribala <parameswaribala@gmail.com>
2020-12-01 23:03:02 +0530

    sagapattern using  cqrs and event sourcing

---
+++ orchestration-saga-pattern-axon-spring-boot/payment-service/src/main/java/com/progressivecoder/paymentmanagement/paymentservice/aggregates/InvoiceAggregate.java
@@ -0,0 +1,37 @@
+package com.progressivecoder.paymentmanagement.paymentservice.aggregates;
+
+import com.progressivecoder.ecommerce.commands.CreateInvoiceCommand;
+import com.progressivecoder.ecommerce.events.InvoiceCreatedEvent;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
+
+@Aggregate
+public class InvoiceAggregate {
+
+    @AggregateIdentifier
+    private String paymentId;
+
+    private String orderId;
+
+    private InvoiceStatus invoiceStatus;
+
+    public InvoiceAggregate() {
+    }
+
+    @CommandHandler
+    public InvoiceAggregate(CreateInvoiceCommand createInvoiceCommand){
+    	System.out.println("Payment-Service ----> CreateInvoiceCommand");
+        AggregateLifecycle.apply(new InvoiceCreatedEvent(createInvoiceCommand.paymentId, createInvoiceCommand.orderId));
+    }
+
+    @EventSourcingHandler
+    protected void on(InvoiceCreatedEvent invoiceCreatedEvent){
+    	System.out.println("Payment-Service ----> InvoiceCreatedEvent");
+        this.paymentId = invoiceCreatedEvent.paymentId;
+        this.orderId = invoiceCreatedEvent.orderId;
+        this.invoiceStatus = InvoiceStatus.PAID;
+    }
+}
